930248361c94583aba6062fd3c6006d0cad14147
Fabiano Sidler <fabiano.sidler@fipscode.net>
2021-08-02 00:12:15 +0200

    waiting for connection opened

--- server.py
+++ server.py
@@ -5,11 +5,12 @@ class Server:
 	def __init__(self, url):
 		ws = WebSocket(url)
 		ws.bind('open',    self.open)
+		self._socket = ws
+	def open(self, event):
+		ws = self._socket
 		ws.bind('message', self.message)
 		ws.bind('close',   self.close)
 		ws.bind('error',   self.error)
-		self._socket = ws
-	def open(self, event): pass
 	def send(self, message):
 		self._socket.send(message)
 	def recv(self, message): pass
